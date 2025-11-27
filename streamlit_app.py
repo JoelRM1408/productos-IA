5b39ae69b196635e8710876ca203552032c01b70
JoelRM1408 <joelito_1408@hotmail.com>
2025-11-27 16:53:06 -0500

    se agrego

--- streamlit_app.py
+++ streamlit_app.py
@@ -315,6 +315,9 @@ if st.session_state.page == "rna":
 
             st.session_state.show_save = True
 
+            st.markdown("<h3 class='estrategias-title'>Estrategias de mejora</h3>", unsafe_allow_html=True)
+            st.markdown("<p class='estrategias-content'>Matar a Andres</p>", unsafe_allow_html=True)
+
         if st.session_state.get("show_save", False):
             if st.button("Guardar", type="primary", use_container_width=True):
                 pred_r = predict_record(model_r, pre_r, schema_r, map_r, entrada)
